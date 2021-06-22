48d69fb401222f73382dfbb988d53cf09d732127
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-06-22 19:06:08 -0400

    added CustomMap so that other programmers can't mess things up

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { User } from "./User";
 import { Company } from "./Company";
 
-new google.maps.Map(document.getElementById("map"), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+import { CustomMap } from "./CustomMap";
+
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap("map");
+
+customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
